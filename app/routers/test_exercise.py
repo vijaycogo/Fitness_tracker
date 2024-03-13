d7728ca3aee4b23e3d395156e91cf63a5bc95ac6
shreekanti <shreekanti97@gmail.com>
2024-03-13 15:56:30 +0530

    unit test and requirement change

---
+++ app/routers/test_exercise.py
@@ -0,0 +1,152 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from fastapi.testclient import TestClient
+from app.main import app
+from app.enum.exercise_enum import IndoorOutdoorExerciseType, MeasurementType, MajorMinorExerciseType
+from app.enum.user_enum import UserRole
+
+class TestExerciseEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.client = TestClient(app)
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.exercise.create')
+    def test_create_exercise(self, mock_create_exercise, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_verify_token.return_value = mock_user
+
+        # Mock the create_exercise function
+        mock_user_data = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_user_data.name = "shree"
+        mock_exercise_data = MagicMock(id=1, exercise_name="Running", exercise_type=IndoorOutdoorExerciseType.outdoors, measurement_type=MeasurementType.time, 
+                                       major_minor_type=MajorMinorExerciseType.mazor, added_by="admin", user=mock_user_data)
+        mock_create_exercise.return_value = mock_exercise_data
+        exercise_data = {
+            "exercise_name": "Running",
+            "exercise_type": "outdoors",
+            "measurement_type": "time",
+            "major_minor_type": "mazor",
+            "per_count_second_unit_calorie": 10,  
+            "added_by": "admin" 
+        }
+
+        # Make a request to the /exercise/ endpoint with a valid token
+        response = self.client.post("/exercise/", json=exercise_data, headers={"Authorization": "Bearer valid_token"})
+
+        # Assert that the response status code is 201
+        self.assertEqual(response.status_code, 201)
+
+        created_exercise = response.json()
+        self.assertEqual(created_exercise["exercise_name"], "Running")
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.exercise.get_all')
+    def test_get_all_exercises(self, mock_get_all_exercises, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_verify_token.return_value = mock_user
+
+        # Mock the exercise items returned by the database
+        mock_user_data = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_user_data.name = "shree"
+        mock_exercise_items = [
+            MagicMock(id=1, exercise_name="Running", exercise_type=IndoorOutdoorExerciseType.outdoors, measurement_type=MeasurementType.time,
+                       major_minor_type=MajorMinorExerciseType.mazor, added_by="admin", user=mock_user_data),
+            MagicMock(id=2, exercise_name="Walking", exercise_type=IndoorOutdoorExerciseType.indoors, measurement_type=MeasurementType.time,
+                       major_minor_type=MajorMinorExerciseType.minor, added_by="admin", user=mock_user_data)
+        ]
+        mock_get_all_exercises.return_value = mock_exercise_items
+
+        # Make a request to the /exercise/ endpoint with a valid token
+        response = self.client.get("/exercise/", headers={"Authorization": "Bearer valid_token"})
+
+        # Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        
+        returned_exercises = response.json()
+
+    
+        self.assertEqual(len(returned_exercises), 2)
+        self.assertEqual(returned_exercises[0]["exercise_name"], "Running")
+        self.assertEqual(returned_exercises[1]["exercise_name"], "Walking")
+        self.assertEqual(returned_exercises[0]["measurement_type"], "time")
+        
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.exercise.show')
+    def test_get_exercise(self, mock_get_exercise, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_verify_token.return_value = mock_user
+
+        # Mock the exercise items returned by the database
+        mock_user_data = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_user_data.name = "shree"
+        mock_exercise_items = MagicMock(id=1, exercise_name="Running", exercise_type=IndoorOutdoorExerciseType.outdoors, measurement_type=MeasurementType.time,
+                       major_minor_type=MajorMinorExerciseType.mazor, added_by="admin", user=mock_user_data)
+        
+        mock_get_exercise.return_value = mock_exercise_items
+
+        # Make a request to the /exercise/ endpoint with a valid token
+        response = self.client.get("/exercise/1", headers={"Authorization": "Bearer valid_token"})
+
+        # Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+         # Assert that the response JSON contains the expected exercise data
+        returned_exercise = response.json()
+        self.assertEqual(returned_exercise["id"], 1)
+        self.assertEqual(returned_exercise["exercise_name"], "Running")
+        self.assertEqual(returned_exercise["exercise_type"], "outdoors")
+        self.assertEqual(returned_exercise["measurement_type"], "time")
+        self.assertEqual(returned_exercise["per_count_second_unit_calorie"], 1)
+
+        
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.exercise.update')
+    def test_update_exercise(self, mock_update_exercise, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role="admin")
+        mock_verify_token.return_value = mock_user
+
+
+        mock_update_exercise.return_value = None
+
+        update_data = {
+            "exercise_name": "Running1",
+            "exercise_type": "indoors",
+            "measurement_type": "time",
+            "per_count_second_unit_calorie": 10,
+            "added_by": "admin",
+            "major_minor_type": "minor"
+        }
+
+        
+        response = self.client.put("/exercise/1", json=update_data, headers={"Authorization": "Bearer valid_token"})
+    
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.text.strip(), '"Exercise updated successfully"')
+        
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.exercise.destroy')
+    def test_delete_user(self, mock_destroy_exercise, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+
+        # Mock the destroy_user function
+        mock_destroy_exercise.return_value = None
+
+        response = self.client.delete("/exercise/1", headers={"Authorization": "Bearer valid_token"})
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.text, "")
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/routers/test_user.py
@@ -0,0 +1,129 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from fastapi.testclient import TestClient
+from app.main import app
+from app.repository import user
+from app.enum.user_enum import UserRole
+from fastapi import Depends, status
+from sqlalchemy.orm import Session
+from app import oauth2
+
+class TestUserEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.client = TestClient(app)
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.user.show')
+    def test_get_user(self, mock_show_user, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+
+        # Mock the show_user function
+        mock_user_data = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_user_data.name = "shree"
+        mock_show_user.return_value = mock_user_data
+
+        # Make a request to the /user/{id} endpoint with a valid token
+        response = self.client.get("/user/1", headers={"Authorization": "Bearer valid_token"})
+
+        # Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the response JSON matches the expected data
+        expected_data = {
+            "id": 1,
+            "name": "shree",
+            "email": "shree@gmail.com",
+            "role": "admin" 
+        }
+        self.assertDictEqual({k: v for k, v in response.json().items() if k in expected_data}, expected_data)
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.user.destroy')
+    def test_delete_user(self, mock_destroy_user, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+
+        # Mock the destroy_user function
+        mock_destroy_user.return_value = None
+
+        response = self.client.delete("/user/1", headers={"Authorization": "Bearer valid_token"})
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.text, "")
+
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.user.update')
+    def test_update_user(self, mock_update_user, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+
+        # Mock the update_user function
+        mock_update_user.return_value = None
+
+        update_data = {
+        "name": "ShreeKanti",
+        "email": "shreekanti@gmail.com",
+        "role": "admin",  
+        "password": "shree1"  
+    }
+
+        
+        response = self.client.put("/user/1", json=update_data, headers={"Authorization": "Bearer valid_token"})
+    
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.text.strip(), '"User updated successfully"')
+
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.user.create')
+    def test_create_user(self, mock_create_user, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+        # Mock the create_user function
+        mock_user_data = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_user_data.name = "shree"
+        mock_create_user.return_value = mock_user_data
+
+        user_data = {
+        "name": "shree",
+        "email": "shree@gmail.com",
+        "role": "admin",
+        "password": "shree" 
+    }
+
+        response = self.client.post("/user/", json=user_data, headers={"Authorization": "Bearer valid_token"})
+        if response.status_code != 200:
+            print(response.json())
+        self.assertEqual(response.status_code, 200)
+        created_user = response.json()
+        self.assertEqual(created_user["name"], "shree")
+
+    @patch('app.oauth2.token.verify_token')
+    @patch('app.repository.user.get_all_user')
+    def test_get_all_users(self, mock_get_all_user, mock_verify_token):
+        # Mock the get_current_user function
+        mock_user = MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        mock_verify_token.return_value = mock_user
+
+        # Mock the get_all_user function
+        mock_user_data = [
+            MagicMock(id=1, name="shree", email="shree@gmail.com", role=UserRole.admin)
+        ]
+        mock_user_data[0].name = "shree"
+        mock_get_all_user.return_value = mock_user_data
+
+        response = self.client.get("/user/", headers={"Authorization": "Bearer valid_token"})
+        self.assertEqual(response.status_code, 200)
+        users = response.json()
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0]["name"], "shree")
+
+
+if __name__ == '__main__':
+    unittest.main()
